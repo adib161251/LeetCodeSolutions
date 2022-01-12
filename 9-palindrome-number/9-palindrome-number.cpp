f0a9fab081a9d804e75db94d2ad6cf58f21db62b
Adib Arman <58285803+adib161251@users.noreply.github.com>
2022-01-12 17:53:59 +0600

    Time: 28 ms (15.51%), Space: 5.9 MB (31.34%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int num = x;
-        int rev = 0;
-        int digit;
-        do{
-            digit = num % 10;
-            if (rev > INT_MAX/10 || rev == INT_MAX / 10 ) return 0;
-            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 )) return 0;
-            rev = (rev * 10)+digit;
-            num = num / 10;
-        }while(num != 0);
-        
-        if(rev < 0) rev *=-1;
-        
-        cout<<rev<<endl;
-        if(x == rev)
-        {
-            return true;
-        }else{
-            return false;
-        }
-        
+        if (x < 0 || (x % 10 == 0 && x != 0)){
+          return false;
+      }
+        int revernumber = 0;
+          while  (x > revernumber){
+              revernumber = revernumber * 10 + x  % 10;
+              x = x / 10;
+          }
+        return x == revernumber || x == revernumber / 10;
     }
 };
